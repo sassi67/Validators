199a0b8ea5c51db8cf168f2526b97ad86e8141f7
sassi67 <sassi67@libero.it>
2015-05-21 17:02:57 +0200

    bugfix

--- validatordouble.cpp
+++ validatordouble.cpp
@@ -17,5 +17,33 @@ ValidatorDouble::ValidatorDouble(double bottom, double top, int decimals, QDoubl
 //-------------------------------------------------------------------------------------------
 QValidator::State ValidatorDouble::validateThis(QString &s, int &pos) const
 {
-    return validator_->validate(s, pos);
+    Q_UNUSED(pos);
+
+    if (s.isEmpty() || s == "-" || s == "+")
+        return QValidator::Intermediate;
+
+    QLocale locale;
+    bool ok;
+    double d = locale.toDouble(s, &ok);
+    if (!ok)
+        return QValidator::Invalid;
+
+    QChar decimalPoint = locale.decimalPoint();
+    bool hasDecimalPoint = (s.indexOf(decimalPoint) != -1);
+    if (hasDecimalPoint)
+    {
+        int charsAfterPoint = s.length() - s.indexOf(decimalPoint) - 1;
+        if (charsAfterPoint > validator_->decimals())
+            return QValidator::Invalid;
+        if (charsAfterPoint == validator_->decimals())
+        {
+            if (d == 0 && (d < validator_->bottom() || d > validator_->top()))
+                return QValidator::Invalid;
+        }
+    }
+
+    if (d >= validator_->bottom() && d <= validator_->top())
+        return QValidator::Acceptable;
+    else
+        return QValidator::Invalid;
 }
